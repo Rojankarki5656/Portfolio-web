f0df17ceea770caa0200e65bd058a368c475495e
Rojan Karki <103635523+Rojankarki5656@users.noreply.github.com>
2025-02-24 17:07:26 +0545

    Delete improved_version directory

--- improved_version/JS/landing.js
+++
@@ -1,8 +0,0 @@
-function checkLoginStatus(destination) {
-    const user = sessionStorage.getItem("userName");
-    if (user) {
-        window.location.href = destination;
-    } else {
-        window.location.href = "login.html";
-    }
-}
